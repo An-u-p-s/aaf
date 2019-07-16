b050e935e4a09b2687351a57f2a3d954c646df30
N.L.N.Rao <nellutlalnrao@gmail.com>
2019-07-16 09:37:31 +0530

    Updated ServiceUtility class with AlphaNumeric Generate Code

--- aaf-springboot-builder/src/main/java/anups/aaf/builder/util/ServiceUtility.java
+++ aaf-springboot-builder/src/main/java/anups/aaf/builder/util/ServiceUtility.java
@@ -1,6 +1,8 @@
 package anups.aaf.builder.util;
 
 
+import java.security.SecureRandom;
+
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.ui.Model;
 
@@ -14,6 +16,14 @@ public class ServiceUtility {
 		urlBuilder.append(request.getContextPath());
 	 model.addAttribute("PROJECT_URL",urlBuilder.toString());
 	return model; 
+ }
+ 
+ public String getAlphaNumeric(int len) {
+	 char[] ch = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".toCharArray();
+	 char[] c = new char[len];
+	 SecureRandom secureRandom = new SecureRandom();
+	 for(int i=0;i<len;i++) {  c[i] =ch[secureRandom.nextInt(ch.length)]; }
+	 return new String(c);
  }
  
 }
